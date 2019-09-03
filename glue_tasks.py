4aa435f3563f6a78a3d76be330cad0f0334fa77d
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-03 17:57:52 +0530

    Update glue_tasks.py

--- glue_tasks.py
+++ glue_tasks.py
@@ -2,7 +2,7 @@ from functools import partial
 
 import torch.nn.functional as F
 from emmental.scorer import Scorer
-from emmental.task import EmmentalTask
+from emmental.task import EmmentalTask, Operation
 from modules.bert_module import BertModule, BertLastCLSModule
 from task_config import LABEL_MAPPING, METRIC_MAPPING
 from torch import nn
@@ -58,23 +58,30 @@ def get_gule_task(task_names, bert_model_name, last_hidden_dropout_prob=0.0):
                     f"{task_name}_feature": BertLastCLSModule(
                     dropout_prob=last_hidden_dropout_prob
                     ),
+                    f"{task_name}_pred_head": nn.Linear(bert_output_dim, task_cardinality),
+
                 }
             ),
             task_flow=[
-                {
-                    "name": "input",
-                    "module": "bert_module",
-                    "inputs": [
-                        ("_input_", "token_ids"),
-                        ("_input_", "token_segments"),
-                        ("_input_", "token_masks"),
-                    ],
-                },
-                {
-                    "name": f"{task_name}_pred_head",
-                    "module": f"{task_name}_pred_head",
-                    "inputs": [("input", 1)],
-                },
+                Operation(
+                name=f"{task_name}_bert_module",
+                module_name="bert_module",
+                inputs=[
+                    ("_input_", "token_ids"),
+                    ("_input_", "token_segments"),
+                    ("_input_", "token_masks"),
+                       ],
+                ),
+            Operation(
+                name=f"{task_name}_feature",
+                module_name=f"{task_name}_feature",
+                inputs=[(f"{task_name}_bert_module", 0)],
+            ),
+            Operation(
+                name=f"{task_name}_pred_head",
+                module_name=f"{task_name}_pred_head",
+                inputs=[(f"{task_name}_feature", 0)],
+            ),
             ],
             loss_func=loss_fn,
             output_func=partial(output, task_name),
