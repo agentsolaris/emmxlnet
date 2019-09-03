45edccd1d98112947d638601cdd811fef20e1501
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-03 17:43:06 +0530

    Update bert_module.py

--- modules/bert_module.py
+++ modules/bert_module.py
@@ -26,3 +26,29 @@ class BertModule(nn.Module):
         #pooled_output = self.dropout(pooled_output)
         #logits = self.classifier(pooled_output)
         return encoded_layers, pooled_output
+
+class BertLastCLSModule(nn.Module):
+    def __init__(self, dropout_prob=0.0):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout_prob)
+
+    def forward(self, input):
+        last_hidden = input[-1][:,0,:]
+        out = self.dropout(last_hidden)
+        return out
+
+
+class BertContactLastCLSWithTwoTokensModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input, idx1, idx2):
+        last_layer = input[-1]
+        last_cls = last_layer[:, 0, :]
+        idx1 = idx1.unsqueeze(-1).unsqueeze(-1).expand([-1, -1, last_layer.size(-1)])
+        idx2 = idx2.unsqueeze(-1).unsqueeze(-1).expand([-1, -1, last_layer.size(-1)])
+        token1_emb = last_layer.gather(dim=1, index=idx1).squeeze(dim=1)
+        token2_emb = last_layer.gather(dim=1, index=idx2).squeeze(dim=1)
+        output = torch.cat([last_cls, token1_emb, token2_emb], dim=-1)
+        return output
+
