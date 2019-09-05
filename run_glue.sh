b3aa9d7f103c49a79fb21875d8162706cba0ca08
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-05 19:45:38 +0530

    Update run_glue.sh

--- run_glue.sh
+++ run_glue.sh
@@ -11,9 +11,9 @@
 GLUEDATA=${2:-data}
 LOGPATH=${3:-logs}
 SEED=${4:-111}
-GPU= 0
-MODEL="xlnet-base-cased"
-NEPCOHS = 3
+GPU= ${5:0}
+MODEL=${6:"xlnet-base-cased"}
+NEPCOHS = ${7:3}
 
 # 
 python run.py \
