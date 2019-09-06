d8c57fbf654ee78ad0eb0e7aaa60a441ea24e389
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-07 03:37:48 +0530

    Update MRPC_sfs.py

--- slicing/MRPC_sfs.py
+++ slicing/MRPC_sfs.py
@@ -10,50 +10,50 @@ def slice_base(example):
     return 1
 
 
-@slicing_function(fields=["sentence1", "sentence2"])
+@slicing_function(fields=["#1 String", "#2 String"])
 def slice_temporal_preposition(example):
     temporal_prepositions = ["after", "before", "past"]
     both_sentences = example.sentence1 + example.sentence2
     return any([p in both_sentences for p in temporal_prepositions])
 
 
-@slicing_function(fields=["sentence1", "sentence2"])
+@slicing_function(fields=["#1 String", "#2 String"])
 def slice_possessive_preposition(example):
     possessive_prepositions = ["inside of", "with", "within"]
     both_sentences = example.sentence1 + example.sentence2
     return any([p in both_sentences for p in possessive_prepositions])
 
 
-@slicing_function(fields=["sentence1", "sentence2"])
+@slicing_function(fields=["#1 String", "#2 String"])
 def slice_is_comparative(example):
     comparative_words = ["more", "less", "better", "worse", "bigger", "smaller"]
     both_sentences = example.sentence1 + example.sentence2
     return any([p in both_sentences for p in comparative_words])
 
 
-@slicing_function(fields=["sentence1", "sentence2"])
+@slicing_function(fields=["#1 String", "#2 String"])
 def slice_is_quantification(example):
     quantification_words = ["all", "some", "none"]
     both_sentences = example.sentence1 + example.sentence2
     return any([p in both_sentences for p in quantification_words])
 
 
-@slicing_function(fields=["sentence2"])
+@slicing_function(fields=["#2 String"])
 def slice_short_hypothesis(example, thresh=5):
     return len(example.sentence2.split()) < thresh
 
 
-@slicing_function(fields=["sentence2"])
+@slicing_function(fields=["#2 String"])
 def slice_long_hypothesis(example, thresh=15):
     return len(example.sentence2.split()) > thresh
 
 
-@slicing_function(fields=["sentence1"])
+@slicing_function(fields=["#1 String"])
 def slice_short_premise(example, thresh=10):
     return len(example.sentence1.split()) < thresh
 
 
-@slicing_function(fields=["sentence1"])
+@slicing_function(fields=["#1 String"])
 def slice_long_premise(example, thresh=100):
     return len(example.sentence1.split()) > thresh
 
