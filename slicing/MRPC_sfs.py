da31b042c47d56213fa147f8581289d04d8ac741
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-07 03:47:20 +0530

    Update MRPC_sfs.py

--- slicing/MRPC_sfs.py
+++ slicing/MRPC_sfs.py
@@ -6,11 +6,6 @@ logger = logging.getLogger(__name__)
 
 
 @slicing_function()
-def slice_base(example):
-    return 1
-
-
-@slicing_function()
 def slice_temporal_preposition(example):
     temporal_prepositions = ["after", "before", "past"]
     both_sentences = example['#1 String'] + example['#2 String']
@@ -58,8 +53,60 @@ def slice_long_premise(example, thresh=100):
     return len(example['#1 String'].split()) > thresh
 
 
+@slicing_function()
+def slice_where(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "where" in sentences
+
+
+@slicing_function()
+def slice_who(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "who" in sentences
+
+
+@slicing_function()
+def slice_what(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "what" in sentences
+
+
+@slicing_function()
+def slice_when(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "when" in sentences
+
+
+@slicing_function()
+def slice_and(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "and" in sentences
+
+
+@slicing_function()
+def slice_but(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "but" in sentences
+
+
+@slicing_function()
+def slice_or(example):
+    sentences = example['#1 String'] + example['#2 String']
+    return "or" in sentences
+
+
+@slicing_function()
+def slice_multiple_articles(example):
+    sentences = example['#1 String'] + example['#2 String']
+    multiple_indefinite = (
+        sum([int(x == "a") for x in sentences.split()]) > 1
+        or sum([int(x == "an") for x in sentences.split()]) > 1
+    )
+    multiple_definite = sum([int(x == "the") for x in sentences.split()]) > 1
+    return multiple_indefinite or multiple_definite
+
+
 slices = [
-    slice_base,
     slice_temporal_preposition,
     slice_possessive_preposition,
     slice_is_comparative,
@@ -68,6 +115,14 @@ slices = [
     slice_long_hypothesis,
     slice_short_premise,
     slice_long_premise,
+    slice_where,
+    slice_who,
+    slice_what,
+    slice_when,
+    slice_and,
+    slice_or,
+    slice_but,
+    slice_multiple_articles,
 ]
 
 slice_func_dict = {slice.__name__: slice for slice in slices}
