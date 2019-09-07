995bfbf3f0b0d32383774889b4efb6c523f7247a
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-08 02:15:41 +0530

    Update run_glue.sh

--- run_glue.sh
+++ run_glue.sh
@@ -29,7 +29,7 @@ python run.py \
   --counter_unit epoch \
   --evaluation_freq 0.5 \
   --checkpoint_freq 1 \
-  --checkpointing 1 \
+  --checkpointing 0 \
   --checkpoint_metric model/train/all/loss:min \
   --checkpoint_task_metrics CoLA/GLUE/dev/matthews_corrcoef:max,MNLI/GLUE/dev/accuracy:max,MRPC/GLUE/dev/accuracy_f1:max,QNLI/GLUE/dev/accuracy:max,QQP/GLUE/dev/accuracy_f1:max,RTE/GLUE/dev/accuracy:max,SNLI/GLUE/dev/accuracy:max,SST-2/GLUE/dev/accuracy:max,STS-B/GLUE/dev/pearson_spearman:max,WNLI/GLUE/dev/accuracy:max \
   --checkpoint_runway 0.5 \
