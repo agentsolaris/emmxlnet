2770b8ab55578b99341ec4288432b2f101435d21
Eduard Barbulescu <eduard.barbulescu@gmail.com>
2019-09-08 02:45:43 +0530

    Update run_glue.sh

--- run_glue.sh
+++ run_glue.sh
@@ -19,7 +19,7 @@ python run.py \
   --data_dir ${GLUEDATA} \
   --log_path ${LOGPATH} \
   --device ${GPU} \
-  --n_epochs 1 \
+  --n_epochs 7 \
   --train_split train \
   --valid_split dev \
   --optimizer adam \
